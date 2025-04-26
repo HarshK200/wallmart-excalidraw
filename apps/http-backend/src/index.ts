d45998117d384a73035955ed74538c28120b6996
HarshK200 <harshkapadia04@gmail.com>
2025-04-26 15:15:07 +0530

    fixed imports of packages/db for http-backend and ws-backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,3 +1,4 @@
+import { prisma } from "@repo/db";
 import express from "express";
 
 const app = express();
@@ -6,6 +7,16 @@ app.get("/", (req, res) => {
   res.json({ msg: "Hi i'm the root route" });
 });
 
+app.get("/db-health", async (req, res) => {
+  try {
+    await prisma.$connect();
+    res.json({ msg: "Successfully connected to the db 🟢" }).status(200);
+  } catch (e) {
+    res.json({ err: "Couldn't connect to db 🔴" }).status(500);
+    console.log("Err:", e);
+  }
+});
+
 app.listen(8080, () => {
-  console.log("server running on port 3000");
+  console.log("server running on port 8080");
 });
