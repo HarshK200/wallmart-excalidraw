3d81722e8d22626e6cdf0a5a6c5abf9c64f7d339
HarshK200 <harshkapadia04@gmail.com>
2025-04-27 14:38:40 +0530

    added simple error handleing to createRoomController

--- apps/http-backend/src/controllers/rooms.ts
+++ apps/http-backend/src/controllers/rooms.ts
@@ -5,28 +5,33 @@ export async function createRoomController(
   req: express.Request,
   res: express.Response,
 ) {
-  const roomDataParsed = createRoomSchema.safeParse({
-    ownerId: req.userId,
-    slug: req.body?.slug,
-  });
-  if (!roomDataParsed.success) {
-    res
-      .status(422)
-      .json({ msg: "Invalid fields", error: roomDataParsed.error });
-    return;
+  try {
+    const roomDataParsed = createRoomSchema.safeParse({
+      ownerId: req.userId,
+      slug: req.body?.slug,
+    });
+    if (!roomDataParsed.success) {
+      res
+        .status(422)
+        .json({ msg: "Invalid fields", error: roomDataParsed.error });
+      return;
+    }
+
+    // checking if room already exists
+    const existingRoom = await prisma.room.findUnique({
+      where: { slug: roomDataParsed.data.slug },
+    });
+    if (existingRoom) {
+      res.status(411).json({ error: "Room with same name already exists" });
+      return;
+    }
+
+    // create new room
+    const room = await prisma.room.create({ data: roomDataParsed.data });
+
+    res.status(201).json({ msg: "room created successfully", roomId: room.id });
+  } catch (e) {
+    res.status(500).json({ error: "Internal server error" });
+    console.log(`Error occured in createRoomController\n\n${e}`);
   }
-
-  // checking if room already exists
-  const existingRoom = await prisma.room.findUnique({
-    where: { slug: roomDataParsed.data.slug },
-  });
-  if (existingRoom) {
-    res.status(411).json({ error: "Room with same slug already exists" });
-    return;
-  }
-
-  // create new room
-  const room = await prisma.room.create({ data: roomDataParsed.data });
-
-  res.status(201).json({ msg: "room created successfully", roomId: room.id });
 }
